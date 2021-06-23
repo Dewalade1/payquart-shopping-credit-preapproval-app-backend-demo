875da428a00665c92d2f1a54595d804cf52af7f2
Dewa <adewalade@gmail.com>
2021-06-23 13:59:10 +0100

    feat: add shoppingCredits router and controller

---
+++ controllers/shoppingCredits.controller.js
@@ -0,0 +1,32 @@
+const shoppingCreditsService = require('../services/shoppingCredits.service')
+
+exports.apply = ( req, res, next ) => {
+    const data = {
+        employmentStatus: req.body.employmentStatus,
+        salary: req.body.salary,
+        nextSalaryDate: req.body.nextSalaryDate,
+        existingLoans: req.body.existingLoans,
+        plan: req.body.plan,
+        shoppingCredit: req.body.shoppingCredit,
+        downPayment: req.body.downPayment,
+        monthlyInstallment: req.body.monthlyInstallment,
+        tenure: req.body.tenure
+    }
+
+    shoppingCreditsService.apply( data, (error, results ) => {
+        if (error) {
+            console.log(error);
+            return res.status(400).send({
+                success: false,
+                data: 'Could not submit application',
+                error: error,
+            })
+        }
+
+        return res.status(200).send({
+            success: true,
+            data: "Application submitted successfully",
+            error: none,
+        })
+    })
+}
--- routes/shoppingCredits.route.js
+++ routes/shoppingCredits.route.js
@@ -1,1 +1,7 @@
 const shoppingCreditsController = require('../controllers/shoppingCredits.controller');
+
+const express = require('express')
+
+const router = express.Router();
+
+router.post('/apply', shoppingCreditsController.apply);
